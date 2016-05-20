0437ced4537a22fd81bd5d571f8966541c9cbe5a
JARMourato <joao.armourato@gmail.com>
2016-05-20 20:05:27 +0100

    Code improvement

--- Sources/Dispatch.swift
+++ Sources/Dispatch.swift
@@ -1,30 +1,32 @@
 /*
-The MIT License (MIT)
-
-Copyright (c) 2016 João Mourato
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ The MIT License (MIT)
+ 
+ Copyright (c) 2016 João Mourato
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 import Foundation
 
 public typealias DispatchClosure = (Void)->(Void)
 public typealias DispatchApplyClosure = (Int)->(Void)
+
+internal var dispatch_time_calc : (NSTimeInterval)->(dispatch_time_t) = { dispatch_time(DISPATCH_TIME_NOW, Int64($0 * NSTimeInterval(NSEC_PER_SEC))) }
 
 public enum Queue{
   
@@ -86,7 +88,7 @@ public struct Group {
     dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
   }
   public func wait(timeout: NSTimeInterval) -> Int {
-    return dispatch_group_wait(group, dispatch_time(DISPATCH_TIME_NOW, Int64(timeout * NSTimeInterval(NSEC_PER_SEC))))
+    return dispatch_group_wait(group, dispatch_time_calc(timeout))
   }
   
 }
@@ -109,7 +111,7 @@ public struct Semaphore {
     dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
   }
   public func wait(timeout: NSTimeInterval) -> Int {
-    return dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, Int64(timeout * NSTimeInterval(NSEC_PER_SEC))))
+    return dispatch_semaphore_wait(semaphore, dispatch_time_calc(timeout))
   }
   
 }
@@ -136,6 +138,9 @@ public enum Dispatch {
   }
   static func apply(iterations: Int, queue: dispatch_queue_t, closure: DispatchApplyClosure) {
     dispatch_apply(iterations, queue, closure)
+  }
+  static func time(timeout: NSTimeInterval) -> dispatch_time_t {
+    return dispatch_time_calc(timeout)
   }
   static var group : Group {
     return Group()
